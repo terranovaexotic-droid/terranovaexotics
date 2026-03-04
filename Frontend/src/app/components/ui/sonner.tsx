9014b9e5c6d61fa18d01bf602c7c1357c23da7c1
Hugo Desjardins <terranovaexotic@gmail.com>
2026-03-04 16:06:35 -0500

    add missing sonner toaster

--- Frontend/src/app/components/ui/sonner.tsx
+++ Frontend/src/app/components/ui/sonner.tsx
@@ -1,25 +1,8 @@
-"use client";
+import * as React from "react";
+import { Toaster as SonnerToaster } from "sonner";
 
-import { useTheme } from "next-themes";
-import { Toaster as Sonner, ToasterProps } from "sonner";
+type ToasterProps = React.ComponentProps<typeof SonnerToaster>;
 
-const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme();
-
-  return (
-    <Sonner
-      theme={theme as ToasterProps["theme"]}
-      className="toaster group"
-      style={
-        {
-          "--normal-bg": "var(--popover)",
-          "--normal-text": "var(--popover-foreground)",
-          "--normal-border": "var(--border)",
-        } as React.CSSProperties
-      }
-      {...props}
-    />
-  );
-};
-
-export { Toaster };
+export function Toaster(props: ToasterProps) {
+  return <SonnerToaster {...props} />;
+}
