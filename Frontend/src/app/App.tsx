ddcbfa62c32c6ed659a49ed1cce6fca5e1dce08b
Hugo Desjardins <terranovaexotic@gmail.com>
2026-03-04 15:58:19 -0500

    fix App router import path

--- Frontend/src/app/App.tsx
+++ Frontend/src/app/App.tsx
@@ -1,6 +1,6 @@
 import { RouterProvider } from "react-router";
-import { router } from "./app/routes";
-import { Toaster } from "./components/ui/sonner";
+import { router } from "./routes";
+import { Toaster } from "../components/ui/sonner";
 
 export default function App() {
   return (
