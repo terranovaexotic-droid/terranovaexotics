0281b849e23451521a9da4c75ab09a2bc8b7772e
Hugo Desjardins <terranovaexotic@gmail.com>
2026-03-04 16:36:28 -0500

    fix router

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,7 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./App";
-  import "./styles/index.css";
-
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
